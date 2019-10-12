8d5fbef0676a664ecf8f4bd9a2de8f81da6a2541
Captain Cold <darkdemon28@gmail.com>
2019-10-12 10:36:26 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+const cors = require ('cors');
+const mysql = require ('mysql');
+const path = require ('path');
+
+const app = express();
+
+app.get('/', function (req, res) {
+
+});
+
+app.listen(3000);
